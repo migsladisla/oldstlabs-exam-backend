3bc1590fdf6707de4c06a9900e53c7bdb4f96452
migsladisla <migsladisla@gmail.com>
2020-10-29 21:07:36 +0800

    Server and database config

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+require('dotenv').config()
+
+const port = process.env.PORT;
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+const buildSchemas = require('./src/schemas');
+
+db.serialize(() => {
+    buildSchemas(db);
+
+    const app = require('./src/app')(db);
+
+    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+});
---
+++ src/schemas.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = (db) => {
+    const createSchema = `
+        CREATE TABLE users
+        (
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        );
+
+        CREATE TABLE appointments
+        (
+            appointment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            comments TEXT NOT NULL,
+            appointment_start_date DATETIME default CURRENT_TIMESTAMP,
+            appointment_end_date DATETIME default CURRENT_TIMESTAMP,
+            created DATETIME default CURRENT_TIMESTAMP,
+            CONSTRAINT fk_users
+                FOREIGN KEY (user_id)
+                    REFERENCES roles(user_id)
+                        ON DELETE CASCADE
+        )
+    `;
+
+    db.exec(createSchema, (err) => {
+        if (err) console.error(err);
+        console.log('Tables created..');
+    });
+
+    return db;
+};
